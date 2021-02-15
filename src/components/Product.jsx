750484a1ca6067d786714deaf8bb0cf434e470d6
Digneli Davila <digneli14@gmail.com>
2021-02-15 14:52:55 -0300

    agregando los componentes necesarios y sus estilos. practica de react hooks, con state, context,  reducer, entre otros

---
+++ src/components/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Product = ({product}) => {
+  return (
+    <div className="Products-item">
+      <img src={product.image} alt={product.title} />
+      <div className="Products-item-info">
+        <h2>
+          {product.price}
+          <span>
+            $
+            {product.price}
+          </span>
+        </h2>
+        <p>{product.description}</p>
+      </div>
+      <button type="button">Comprar</button>
+    </div>
+  );
+}
+
+export default Product;
